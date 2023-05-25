018562d1121014273fbf87a019dbe8c62d295529
Hassaan-25 <hassaan.bs25@gmail.com>
2023-05-26 01:45:39 +0500

    deploy

--- server.js
+++ server.js
@@ -1,38 +1,51 @@
 // const busboy = require('connect-busboy') //middleware for form/file upload
-const debug = require('debug-levels')('server')
-const path = require('path')
-const express = require('express')
-const ModelUtils = require('./models/ModelUtils')
-var jwt = require('jsonwebtoken')
-var bodyParser = require('body-parser')
-const cors = require('cors');
-
+const debug = require("debug-levels")("server");
+const path = require("path");
+const express = require("express");
+const ModelUtils = require("./models/ModelUtils");
+var jwt = require("jsonwebtoken");
+var bodyParser = require("body-parser");
+const cors = require("cors");
 
 // require .env
-require('dotenv').config()
+require("dotenv").config();
 
 // basic
-const DbConn = require('./lib/DbConn')
+const DbConn = require("./lib/DbConn");
 
-const app = require("./routes/Router")
+const app = require("./routes/Router");
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
- 
-// parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
 
-const port = process.env.PORT || 3001
+// parse application/json
+app.use(bodyParser.json());
+
+const port = process.env.PORT || 3001;
+
+const _dirname = path.dirname("");
+const buildPath = path.join(_dirname, "../../my-app/build");
+app.use(express.static(buildPath));
+
+app.get("/*", function (req, res) {
+  res.sendFile(
+    path.join(__dirname, "../../my-app/build/index.html"),
+    function (err) {
+      if (err) {
+        res.status(500).send(err);
+      }
+    }
+  );
+});
 
 async function startUp() {
-    // init models to load db conn
-  await DbConn.init()
-  await ModelUtils.initAll()
+  // init models to load db conn
+  await DbConn.init();
+  await ModelUtils.initAll();
 
   app.listen(port, function () {
-    console.log('fe-backend server listening on port ', port)
-  })
-  
+    console.log("fe-backend server listening on port ", port);
+  });
 }
 
-startUp()
+startUp();
